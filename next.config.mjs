a3c1d19c2e787c917749da7fcdebef86caf9c2ac
mandymerlin <jogriverside@hotmail.com>
2024-07-16 19:46:06 +0100

    added page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    // Your other Next.js configuration options
+    // Example configuration for global CSS
+    style: {
+      // This allows you to include global CSS in your project
+      global: {
+        css: './styles/global.css', // Path to your global CSS file
+      },
+    },
+  };
+  
