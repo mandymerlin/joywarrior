d3d86891e79551566d90c883dc9d1f6f6a9e7212
mandymerlin <jogriverside@hotmail.com>
2024-07-17 07:47:53 +0100

    page add

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
-// next.config.mjs
-export default {
-    // Your other Next.js configuration options
-    // Example configuration for global CSS
-    style: {
-      // This allows you to include global CSS in your project
-      global: {
-        css: './styles/global.css', // Path to your global CSS file
-      },
-    },
-  };
-  
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
