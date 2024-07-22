8e2ed2c1e1cef55f2236e3842275a99f5cbd117b
mandymerlin <jogriverside@hotmail.com>
2024-07-22 15:22:36 +0100

    added contact and training pages

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,13 @@
+// src/app/contact/page.tsx
+import React from 'react';
+
+const ContactPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Emails, zoom calander</p>
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ src/app/training/page.tsx
@@ -0,0 +1,13 @@
+// src/app/training/page.tsx
+import React from 'react';
+
+const TrainingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Training</h1>
+      <p>Please choose how you learn best: Visual, text or video</p>
+    </div>
+  );
+};
+
+export default TrainingPage;
