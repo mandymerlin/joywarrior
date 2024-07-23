09f8bd2333fcb4ba2fb12e56799615fb08827ad4
mandymerlin <jogriverside@hotmail.com>
2024-07-23 11:04:38 +0100

    colour styling

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,9 +1,10 @@
 // src/app/training/page.tsx
 import React from 'react';
+import styles from './TrainingPage.module.css';
 
 const TrainingPage: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Training</h1>
       <p>Please choose how you learn best: Visual, text or video</p>
     </div>
