3fde8965d6b3cad8f9a0791edf3738109c369c3b
mandymerlin <jogriverside@hotmail.com>
2024-07-23 11:08:42 +0100

    css pages added

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,9 +1,11 @@
 // src/app/contact/page.tsx
 import React from 'react';
+import styles from './ContactPage.module.css';
 
 const ContactPage: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
+  
       <h1>Contact</h1>
       <p>Emails, zoom calander</p>
     </div>
