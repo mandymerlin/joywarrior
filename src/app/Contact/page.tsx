3477864c748ed0a244cdfd934fa4c767b3c4270f
mandymerlin <jogriverside@hotmail.com>
2024-07-29 15:54:59 +0100

    contactlist

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -5,8 +5,7 @@ import styles from './ContactPage.module.css';
 const ContactPage: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Contact</h1>
-      <p>Emails, zoom calander</p>
+      <img src="/images/contact.jpg" alt="contacts" className={styles.image} />
     </div>
   );
 };
